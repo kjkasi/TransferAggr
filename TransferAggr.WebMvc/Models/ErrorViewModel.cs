a9a9bd9238590a2af94587720b7e57bc1430f288
kjkasi <5842602+kjkasi@users.noreply.github.com>
2023-01-25 19:16:12 +0300

    Add project WebMvc

---
+++ TransferAggr.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TransferAggr.WebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
